30da65dc901a15d10ec7f88c3da2101b547f5e4f
nourr-ahmed <nourr.ahmedd10@gmail.com>
2026-09-16 21:35:16 +0300

    Add ExpenseSerializer

---
+++ backend/app/serializers/expense_serializer.rb
@@ -0,0 +1,64 @@
+class ExpenseSerializer
+  def initialize(expense)
+    @expense = expense
+  end
+
+  def as_json
+    {
+      id: @expense.id,
+      title: @expense.title,
+      description: @expense.description,
+      amount: @expense.amount,
+      status: @expense.status,
+      spent_on: @expense.spent_on,
+      created_at: @expense.created_at,
+      updated_at: @expense.updated_at,
+      category: category_data,
+      owner: owner_data,
+      history: history_data
+    }
+  end
+
+  private
+
+  def category_data
+    {
+      id: @expense.category.id,
+      name: @expense.category.name,
+      auto_approve_limit: @expense.category.auto_approve_limit
+    }
+  end
+
+  def owner_data 
+    {
+      id: @expense.user.id,
+      name: @expense.user.name,
+      role: @expense.user.role
+    }
+  end
+
+  def history_data
+    @expense.expense_histories.order(:created_at).map do |h|
+      {
+        from_status: h.from_status,
+        to_status: h.to_status,
+        comment: h.comment,
+        created_at: h.created_at,
+        actor: actor_data(h)
+      }
+    end
+
+    def actor_data(history)
+      if history.actor_user
+        {
+          id: history.actor_user.id,
+          name: history.actor_user.name
+        }
+      else 
+        {
+          name: "system"
+        }
+      end
+    end
+
+end
